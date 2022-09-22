d8c59ab60941e2c20da63e6f5d475059c784a623
Tushar <singhtushar348@gmail.com>
2022-09-22 17:48:56 +0100

    Add test cases as comments and fix offenses

--- my_list.rb
+++ my_list.rb
@@ -11,6 +11,19 @@ class MyList
   end
 end
 
-list = MyList.new(1, 2, 3, 4)
-list.each { |a| puts(a) }
-puts(list.all? { |e| e > 5 })
+# test cases
+# Create our list
+# list = MyList.new(1, 2, 3, 4)
+# <MyList: @list=[1, 2, 3, 4]>
+
+# Test #all?
+# puts(list.all? { |e| e < 5 })
+# puts(list.all? { |e| e > 5 })
+# puts(list.all? { |e| e.even? })
+
+# Test #any?
+# puts(list.any? { |e| e == 2 })
+# puts(list.any? { |e| e == 5 })
+
+# Test #filter
+# puts(list.filter { |e| e.even? })
